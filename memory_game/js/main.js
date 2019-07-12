a044bbc38a68dcfe657379998cf01f31e18fab13
Joshua Hunter <joshuahunter@Marisas-Air.domain>
2019-07-12 11:14:50 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,15 @@
+//create four variables that simulate the cards in your game
+//string data types
+//two "queen" values and two "king" values
+//name variables cardOne, cardTwo, cardThree, cardFour
+//console.log("User flipped " + cardOne); repeat through cardFour
+
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
